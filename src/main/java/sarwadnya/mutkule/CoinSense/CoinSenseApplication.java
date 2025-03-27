b269ffa3eecd4333982e714a1db6ebd022544aa2
Sarwadnya <sarwadnyacoding@gmail.com>
2025-03-28 03:47:03 +0530

    Basic Setup for Ex[ecse Tracker

---
+++ src/main/java/sarwadnya/mutkule/CoinSense/CoinSenseApplication.java
@@ -0,0 +1,14 @@
+package sarwadnya.mutkule.CoinSense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinSenseApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CoinSenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sarwadnya/mutkule/CoinSense/CoinSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package sarwadnya.mutkule.CoinSense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinSenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
